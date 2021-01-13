a9797eb721fbed46edaa1ec848186d1f64834ca8
Your Name <you@example.com>
2021-01-13 10:46:55 +0700

    create

---
+++ testFor.php
@@ -0,0 +1,30 @@
+<html>
+    
+
+<body>
+<center>
+<?php
+for($i=1 ;$i<=7;$i++){
+
+    echo ("<font size =\"".$i."\">Welcome To PHP WORLD </font><br/>");
+    
+}
+
+
+?>
+</center>
+
+
+
+
+
+</body>
+
+
+
+
+
+
+
+    </html>
+
