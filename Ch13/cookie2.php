b240a8c493620a586ab2010140ab7612a487cdae
akegametime <52166668+akegametime@users.noreply.github.com>
2021-03-24 12:08:23 +0700

    Add files via upload

---
+++ Ch13/cookie2.php
@@ -0,0 +1,23 @@
+<?php
+$cookie_name = "user";
+$cookie_value = "Alex";
+
+setcookie($cookie_name,$cookie_value,time()+(86400*30),"/");
+?>
+
+<html>
+
+<body>
+
+<?php
+if(!isset($_COOKIE[$cookie_name])){
+    echo "Cookie named ' ".$cookie_name."' is not set ! ";
+
+} else {
+
+    echo "cookie'".$cookie_name."'is set!<br>";
+    echo "Value is : ".$_COOKIE[$cookie_name];
+
+}
+?>
+</body></html>
